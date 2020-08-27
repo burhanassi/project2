c5d8f1a04265d9f9915d488d72b14c383161cc7d
burhanassi <61707044+burhanassi@users.noreply.github.com>
2020-08-27 08:53:45 +0300

    2nd step of doing the project [using hooks and typeScript(without context)]

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import NumberAgreeForm from "./components/NumberAgreeForm";
 import Summary from "./components/Summary";
 import classes from "./components/Style.module.css";
@@ -17,36 +17,40 @@ function App() {
         setRandomNum(Math.floor(Math.random() * (max - min) + min));
     };
 
-    let redirects = null;
-
-    const submitHandler = (event: React.FormEvent) => {
-        redirects = <Redirect to={'/agree'} from={'/'}/>;
+    const clickHandler = (event: React.FormEvent) => {
+        window.history.pushState(null, '','/agree');
         console.log(number);
+        setNumber(-1);
     };
 
     const SubmitForm = (<div className={classes.SubmitFormControl}>
-        <form onSubmit={submitHandler} className={classes.SubmitForm} >
+        <div className={classes.SubmitForm} >
             <Typography className={classes.Item} variant="h4" gutterBottom>
                 Enter a number from 0 to 100
             </Typography>
-            <TextField className={classes.Item} onChange={(e) => setNumber(parseInt(e.target.value) || number)} name={"number"} type={"number"} id="name" label="Your Number" variant="outlined" />
-            <Button onClick={() => <Redirect to={'/agree'}/>} className={classes.Item} type={"submit"} variant="contained" color="primary">
+            <TextField className={classes.Item} onChange={(e) => {
+                if(parseInt(e.target.value) >= 0 && parseInt(e.target.value)  <= 100){
+                    setNumber(parseInt(e.target.value) || number)
+                }else {
+                    window.alert("Please Enter A Number from 0 to 100");
+                }
+            }} name={"number"} type={"number"} id="name" label="Your Number" variant="outlined" />
+            <Button className={classes.Item} onClick={clickHandler} variant="contained" color="primary">
                 Submit
             </Button>
-        </form>
+        </div>
     </div>);
 
     let routes = <Switch>
-        <Route exact path={'/'} render={() => SubmitForm}/>
-        <Route exact path={'/agree'} render={() => <NumberAgreeForm num={number} randNum={randomNum} numbersList={numbersList} buttonHandler={buttonHandler}/>}/>
         <Route exact path={'/summary'} render={() => <Summary items={numbersList} num={number}/>}/>
+        <Route exact path={'/agree'} render={() => <NumberAgreeForm num={number} randNum={randomNum} numbersList={numbersList} buttonHandler={buttonHandler}/>}/>
+        <Route exact path={'/'} render={() => SubmitForm}/>
     </Switch>
 
   return (
     <div>
         <BrowserRouter>
             {routes}
-            {redirects}
         </BrowserRouter>
     </div>
   );
